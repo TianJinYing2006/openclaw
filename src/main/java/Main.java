283eefbaef56539ce4bf4719de89af37160f9075
wangentong <wangentong50@gmail.com>
2026-07-15 16:36:45 +0800

    xiugai

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // 个人分支练习：程序仍然输出 Hello World
-        System.out.println("Hello World666777");
+        System.out.println("Hello World666777分支");
     }
 }
